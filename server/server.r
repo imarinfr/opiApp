5ed24d0dc404d3378791459e980124b3131445a1
Iván Marín-Franch <ivan.marin.franch@gmail.com>
2022-04-25 17:58:54 +0200

    error handling for Compass opiInit, which returns a structure when successfully connected

--- server/server.r
+++ server/server.r
@@ -12,33 +12,33 @@ server <- future({
     # listen for instructions from the GUI. If command is opiIdle, then wait a bit,
     # otherwise, go ahead
     if(!ShinySender$empty()) {
-      msg <- parseMessage(ShinySender$pop()$message, appParams)
-      if(is.null(msg))
+      res <- parseMessage(ShinySender$pop()$message, appParams)
+      if(is.null(res))
         ShinyReceiver$push("ERR", "OPI server: OPI statement badly constructed")
       else {
-        cmd <- msg$cmd
-        pars <- msg$pars
+        cmd <- res$cmd
+        pars <- res$pars
       }
     }
     ################
     # OPI Initialize
     ################
     if(cmd == "opiInit") {
-      msg <- tryCatch(do.call(what = opiInitialize, args = pars), error = function(e) e$message)
-      if(is.null(msg))
+      res <- tryCatch(do.call(what = opiInitialize, args = pars), error = function(e) e$message)
+      if(is.null(res) || is.null(res$err))
         ShinyReceiver$push("OK", paste0("OPI server: OPI initialized for '", chooseOPI()[.OpiEnv$chooser], "'"))
       else
-        ShinyReceiver$push("ERR", paste("OPI server:", msg))
+        ShinyReceiver$push("ERR", paste("OPI server:", res))
     }
     ####################
     # OPI Set Background
     ####################
     if(cmd == "opiSetBackground") {
-      msg <- tryCatch(do.call(what = opiSetBackground, args = pars), error = function(e) e$message)
-      if(is.null(msg))
+      res <- tryCatch(do.call(what = opiSetBackground, args = pars), error = function(e) e$message)
+      if(is.null(res))
         ShinyReceiver$push("OK", "OPI server: background changed")
       else
-        ShinyReceiver$push("ERR", paste("OPI server:", msg))
+        ShinyReceiver$push("ERR", paste("OPI server:", res))
     }
     ###############
     # OPI Test Init
@@ -48,27 +48,27 @@ server <- future({
         locs <- data.frame(x = 0, y = 0, w = 1)
       else
         locs <- grids[[pars$grid]]$locs
-      msg <- tryCatch({
+      res <- tryCatch({
         setup <- testSetup(chooseOPI()[.OpiEnv$chooser], appParams, pars$eye, pars$perimetry,
                               pars$algorithm, pars$val, pars$algpar, locs)
         states <- setup$states
         settings <- setup$settings
         if(!is.null(settings))
-          msg <- NULL
+          res <- NULL
         else
-          msg <- "Wrong test settings"
+          res <- "Wrong test settings"
       }, error = function(e) e$message)
-      if(is.null(msg)) {
+      if(is.null(res)) {
         ShinyReceiver$push("OK", "OPI server: test settings ready")
       } else
-        ShinyReceiver$push("ERR", paste("OPI server:", msg))
+        ShinyReceiver$push("ERR", paste("OPI server:", res))
     }
     ###################
     # OPI Test Step Run
     ###################
     if(cmd == "opiTestStepRun") {
       # go ahead with the step run
-      msg <- tryCatch({
+      res <- tryCatch({
         # perform step
         rs <- testStep(states, settings)
         # update states and settings
@@ -76,28 +76,28 @@ server <- future({
         settings <- rs$settings
         NULL
       }, error = function(e) e$message)
-      if(is.null(msg)) { # if all good, inform, then send results
+      if(is.null(res)) { # if all good, inform, then send results
         ShinyReceiver$push("OK", "OPI server: test step successful")
-        msg <- tryCatch(returnResults(rs$res), error = function(e) e$message)
+        res <- tryCatch(returnResults(rs$res), error = function(e) e$message)
       }
       else
-        ShinyReceiver$push("ERR", paste("OPI server:", msg))
+        ShinyReceiver$push("ERR", paste("OPI server:", res))
     }
     ######################
     # OPI TEST Catch Trial
     ######################
     if(cmd == "opiTestCatchTrial") {
-      msg <- tryCatch({
+      res <- tryCatch({
         # present catch trial
         res <- testCatchTrial(settings, pars)
         NULL
       }, error = function(e) e$message)
-      if(is.null(msg)) { # if all good, inform, then send results
+      if(is.null(res)) { # if all good, inform, then send results
         ShinyReceiver$push("OK", "OPI server: catch trial successful")
-        msg <- tryCatch(returnResults(res), error = function(e) e$message)
+        res <- tryCatch(returnResults(res), error = function(e) e$message)
       }
       else
-        ShinyReceiver$push("ERR", paste("OPI server:", msg))
+        ShinyReceiver$push("ERR", paste("OPI server:", res))
     }
     ##############
     # OPI Test End
@@ -106,10 +106,7 @@ server <- future({
       settings <- NULL
       states <- NULL
       locs <- NULL
-      if(is.null(msg))
-        ShinyReceiver$push("OK", "OPI server: test ended successfully")
-      else
-        ShinyReceiver$push("ERR", paste("OPI server:", msg))
+      ShinyReceiver$push("OK", "OPI server: test ended successfully")
     }
     ###########
     # OPI Close
